700a5c230a0ef9286e8ceb2cd4fb651b2a19a1b8
Joshijay1917 <jayjoshi1912007@gmail.com>
2025-09-11 06:55:22 +0530

    Update App.jsx
    In distance function convert the latest and long to integer

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,29 @@ function App() {
     latitude: ''
   })
 
+  function toRad(x) {
+      return (x * Math.PI) / 180;
+  }
+
   const calculateDistance = (lat1, lon1, lat2, lon2) => {
-    const R = 6371000; // meters
-  const toRad = x => x * Math.PI / 180;
+    lat1 = parseFloat(lat1);
+      lon1 = parseFloat(lon1);
+      lat2 = parseFloat(lat2);
+      lon2 = parseFloat(lon2);
 
-  const dLat = toRad(lat2 - lat1);
-  const dLon = toRad(lon2 - lon1);
+      const R = 6371e3; // metres
+      const φ1 = toRad(lat1);
+      const φ2 = toRad(lat2);
+      const Δφ = toRad(lat2 - lat1);
+      const Δλ = toRad(lon2 - lon1);
 
-  const a = Math.sin(dLat/2) ** 2 +
-            Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-            Math.sin(dLon/2) ** 2;
+      const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
 
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-  return R * c;
+      return R * c;
   }
 
   const counter = () => {
